df408639ded8f92fa394627b55124215cfc62a54
Matthieu Sabourin <matthieusabourin@Matthieus-MacBook-Air.local>
2023-03-11 19:24:39 +0100

    louis v12

--- client/script.js
+++ client/script.js
@@ -88,7 +88,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv)
 
     
-    const response = await fetch('https://rob-ai.onrender.com/', {
+    const response = await fetch('https://rob-ai.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
